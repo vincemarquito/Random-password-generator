f47d9e9d17f40fc33279a50f79445b3e9d4a88e1
Vince <109780641+vincemarquito@users.noreply.github.com>
2024-04-21 22:15:54 +0800

    Add files via upload

---
+++ random_password.js
@@ -0,0 +1,14 @@
+function generatePassword(length) {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  return password;
+}
+
+const password = generatePassword(10); // <--- adjust the password length if you want
+console.log(password);
+
+// Run on console or just link on your html file
